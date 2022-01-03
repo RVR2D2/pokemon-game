462d6782aba40b48d5cac51af3374f330e871b26
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2022-01-03 21:41:59 +0600

    feat(Header, layout, Footer)
    add new component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import bg1 from './assets/bg1.jpeg'
+import bg2 from './assets/bg2.jpeg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello, World ReactJS
-        </p>
-      </header>
-    </div>
-  );
+    <>
+      <Header
+        title='This is title'
+        descr='This is Description!'
+      />
+      <Layout
+        title='title'
+        descr='description'
+        urlBg={bg1}
+      />
+      <Layout
+        title='title'
+        descr='description'
+        colorBg='#e2e2e2'
+      />
+      <Layout
+        title='title'
+        descr='description'
+        urlBg={bg2}
+      />
+      <Footer />
+    </>
+  )
 }
-
 export default App;
